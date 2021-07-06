e129c0c3eaeb31d01c81088cbfbf6d8d21836e03
Rhy <415560124@qq.com>
2021-07-06 17:31:25 +0800

    1.数组和链表实现 2.LRU算法缓存实现 3.约瑟夫问题实现 4.栈实现 5.括号匹配实现

---
+++ src/main/java/com/rhy/datastructuresandalgorithms/stack/ArrayStack.java
@@ -0,0 +1,91 @@
+package com.rhy.datastructuresandalgorithms.stack;
+
+/**
+ * @author: Herion Lemon
+ * @date: 2021年07月06日 15:33:00
+ * @slogan: 如果你想攀登高峰，切莫把彩虹当梯子
+ * @description:
+ */
+public class ArrayStack<T> {
+    private int index;
+    private int size;
+    private T[] data;
+    public ArrayStack(int size){
+        this.data = (T[]) new Object[size];
+        this.size = size;
+        this.index = 0;
+    }
+    /**
+     * 入栈
+     * @param value 值
+     */
+    public void push(T value){
+        //如果当前计数器长度超过了容量
+        if(index >= size){
+            //扩容
+            resize(this.size*2);
+        }
+        //给数据最后一个位置放入值
+        this.data[index++] = value;
+    }
+
+    /**
+     * 扩容
+     * @param size 扩容后大小
+     */
+    public void resize(int size){
+        //扩容
+        T[] newData = (T[]) new Object[size];
+        //循环原数组，复制到新数组
+        for (int i = 0,len = index; i < len; i++) {
+            newData[i] = this.data[i];
+        }
+        //赋值扩容后数组
+        this.data = newData;
+    }
+    /**
+     * 出栈
+     * @return
+     */
+    public T pop(){
+        //如果栈为空，则返回null
+        if(index == 0){
+            return null;
+        }
+        //计数器减1 && 弹出最后进入的元素
+        T data = this.data[--index];
+        //弹出后制空
+        this.data[index] = null;
+        return data;
+    }
+
+    /**
+     * 打印输出
+     */
+    public void print(){
+        for (int i = 0; i < index; i++) {
+            System.out.print(data[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return index == 0;
+    }
+    public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(10);
+        for (int i = 0; i < 20; i++) {
+            arrayStack.push(i);
+        }
+        arrayStack.print();
+        for (int i = 0; i < 21; i++) {
+            System.out.print(arrayStack.pop()+" ");
+        }
+
+    }
+
+}
---
+++ src/main/java/com/rhy/datastructuresandalgorithms/stack/BracketMatch.java
@@ -0,0 +1,57 @@
+package com.rhy.datastructuresandalgorithms.stack;
+
+import org.yaml.snakeyaml.util.ArrayStack;
+
+/**
+ * @author: Herion Lemon
+ * @date: 2021年07月06日 16:54:00
+ * @slogan: 如果你想攀登高峰，切莫把彩虹当梯子
+ * @description: 括号匹配
+ */
+public class BracketMatch {
+    public static void main(String[] args) {
+
+        System.out.println(match("[{}{[())]}]"));
+    }
+
+    public static String match(String str){
+        ArrayStack<Character> stack = new ArrayStack<>(20);
+        char[] chars = str.toCharArray();
+        boolean isError = false;
+        for (int i = 0,len = chars.length; i < len; i++) {
+            char aChar = chars[i];
+            switch (aChar){
+                case '[':
+                case '{':
+                case '(':
+                    stack.push(aChar);
+                    break;
+                default:
+                    Character pop = stack.pop();
+                    switch (pop){
+                        case '[':
+                            if(aChar != ']'){
+                                isError = true;
+                            }
+                            break;
+                        case '{':
+                            if(aChar != '}'){
+                                isError = true;
+                            }
+                            break;
+                        case '(':
+                            if (aChar != ')'){
+                                isError = true;
+                            }
+                            break;
+                        default:
+                            isError = true;
+                    }
+            }
+            if(isError){
+                break;
+            }
+        }
+        return isError ? "异常" : "正确";
+    }
+}
