edff888beedb24bff4a2f3f6a7cd711a50de655c
rhy <415560124@qq.com>
2021-09-04 21:00:49 +0800

    1.快速排序实现

---
+++ src/main/java/com/rhy/datastructuresandalgorithms/sort/QuiklySort.java
@@ -0,0 +1,59 @@
+package com.rhy.datastructuresandalgorithms.sort;
+
+/**
+ * @author: Herion Lemon
+ * @date: 2021/9/4 20:37
+ * @slogan: 如果你想攀登高峰，切莫把彩虹当梯子
+ * @description:
+ */
+public class QuiklySort {
+    public static void main(String[] args) {
+        int[] arr = DataService.getArr();
+        qsort(arr,0,arr.length-1);
+    }
+
+    /**
+     * 快速排序
+     * @param arr 待排序数组
+     * @param start 开始下标
+     * @param end 结束下标
+     */
+    public static void qsort(int[] arr,int start,int end){
+        if(start >= end){
+            return;
+        }
+        //排序位置的开始下标与结束下标
+        System.out.println(start+"-"+end);
+
+        int basic = arr[start];
+        //左标
+        int left = start;
+        //右标
+        int right = end;
+        while (left < right){
+            //从后往前找比基准数小的
+            while (left < right && arr[right] >= basic){
+                right--;
+            }
+            //说明找到了比基准数小的，开始交换位置
+            if(left < right){
+                arr[left] = arr[right];
+                arr[right] = basic;
+                left++;
+            }
+            //从前往后找比基准数大的
+            while (left < right && arr[left] <= basic){
+                left++;
+            }
+            //找到了比基准数大的，开始交换位置
+            if(left < right){
+                arr[right] = arr[left];
+                arr[left] = basic;
+                right--;
+            }
+        }
+        DataService.show(arr);
+        qsort(arr,start,left-1);
+        qsort(arr,left+1,end);
+    }
+}
